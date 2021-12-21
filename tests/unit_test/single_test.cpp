59f0dca90047fc7ade75cec0db2f08fb2ef597ba
winsoft666 <winsoft666@outlook.com>
2021-12-21 13:05:49 +0800

    Fix resource free bug in test cases.

--- tests/unit_test/single_test.cpp
+++ tests/unit_test/single_test.cpp
@@ -6,40 +6,40 @@ using namespace teemo;
 
 TEST(SingleTest, test1) {
   Teemo::GlobalInit();
+  {
+    Teemo efd;
+    efd.setThreadNum(6);
+    efd.setSlicePolicy(SlicePolicy::FixedSize, 1024000 * 5);
+    efd.setHashVerifyPolicy(ALWAYS, MD5, "9b7af5c91139659b10b84b1ca357d08f");
+    efd.setHttpHeaders({{u8"Origin", u8"https://mysql.com"},
+                        {u8"User-Agent", u8"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"}});
 
-  Teemo efd;
-  efd.setThreadNum(6);
-  efd.setSlicePolicy(SlicePolicy::FixedSize, 1024000 * 5);
-  efd.setHashVerifyPolicy(ALWAYS, MD5, "9b7af5c91139659b10b84b1ca357d08f");
-  efd.setHttpHeaders({{u8"Origin", u8"https://mysql.com"},
-                      {u8"User-Agent", u8"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"}});
+    efd.start(
+        "https://cdn.mysql.com//Downloads/MySQLInstaller/mysql-installer-community-8.0.27.1.msi",
+        "mysql-installer-community-8.0.27.1.msi",
+        [](Result result) {
+          printf("\nResult: %s\n", GetResultString(result));
+          EXPECT_TRUE(result == SUCCESSED || result == CANCELED);
+        },
+        [](int64_t total, int64_t downloaded) {
+          if (total > 0)
+            printf("%3d%%\b\b\b\b", (int)((double)downloaded * 100.f / (double)total));
+        },
+        nullptr);
 
-  efd.start(
-      "https://cdn.mysql.com//Downloads/MySQLInstaller/mysql-installer-community-8.0.27.1.msi",
-      "mysql-installer-community-8.0.27.1.msi",
-      [](Result result) {
-        printf("\nResult: %s\n", GetResultString(result));
-        EXPECT_TRUE(result == SUCCESSED || result == CANCELED);
-      },
-      [](int64_t total, int64_t downloaded) {
-        if (total > 0)
-          printf("%3d%%\b\b\b\b", (int)((double)downloaded * 100.f / (double)total));
-      },
-      nullptr);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-  //std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    getchar();
 
-  getchar();
+    efd.stop();
 
-  efd.stop();
+    std::shared_future<Result> future_result = efd.futureResult();
+    if (future_result.valid()) {
+      Result ret = future_result.get();
+      printf("\nResult: %s\n", GetResultString(ret));
+    }
 
-  std::shared_future<Result> future_result = efd.futureResult();
-  if (future_result.valid()) {
-    Result ret = future_result.get();
-    printf("\nResult: %s\n", GetResultString(ret));
+    getchar();
   }
-
-  getchar();
-
   Teemo::GlobalUnInit();
 }

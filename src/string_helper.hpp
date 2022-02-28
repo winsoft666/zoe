087160abd6757cfa8442f1ae609ee76673d64d37
winsoft666 <winsoft666@outlook.com>
2022-02-28 18:52:42 +0800

    Update LICENSE.

--- src/string_helper.hpp
+++ src/string_helper.hpp
@@ -1,16 +1,19 @@
 /*******************************************************************************
-* Copyright (C) 2019 - 2023, winsoft666, <winsoft666@outlook.com>.
+*    Copyright (C) <2019-2022>, winsoft666, <winsoft666@outlook.com>.
 *
-* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
-* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
 *
-* Expect bugs
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
 *
-* Please use and enjoy. Please let me know of any bugs/improvements
-* that you have found/implemented and I will fix/incorporate them into this
-* file.
-*******************************************************************************/
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 
 #ifndef TEEMO_STRING_HELPER_H_
 #define TEEMO_STRING_HELPER_H_

4eca96325fd168791b378a5757999bb392c5ba83
winsoft666 <mioktiar@gmail.com>
2024-11-11 11:33:17 +0800

    Using UTF8-BOM

--- tests/unit_test/test_data.h
+++ tests/unit_test/test_data.h
@@ -1,4 +1,4 @@
-#pragma once
+ï»¿#pragma once
 #include <string>
 #include <vector>
 
@@ -11,12 +11,12 @@ typedef struct _TestData {
 static std::vector<TestData> http_test_datas = {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
     {u8"https://dlie.sogoucdn.com/se/sogou_explorer_8.6_1120.exe",
-     u8"C:/TeemoTest/ËÑ¹·ä¯ÀÀÆ÷_8.6_1120.exe", u8"8e78e77400cdc268032a5491d2fe18d8"},
+     u8"C:/TeemoTest/æœç‹—æµè§ˆå™¨_8.6_1120.exe", u8"8e78e77400cdc268032a5491d2fe18d8"},
     {u8"http://download.ydstatic.cn/cidian/static/8.9/20201217/YoudaoDictSetup.exe",
      u8"C:\\TeemoTest\\YoudaoDictSetup.exe", u8"e3323c940f0bc62fae3b0f32d7f2453b"},
 #else
     {u8"https://dlie.sogoucdn.com/se/sogou_explorer_8.6_1120.exe",
-     u8"~/TeemoTest/ËÑ¹·ä¯ÀÀÆ÷_8.6_1120.exe", u8"8e78e77400cdc268032a5491d2fe18d8"},
+     u8"~/TeemoTest/æœç‹—æµè§ˆå™¨_8.6_1120.exe", u8"8e78e77400cdc268032a5491d2fe18d8"},
     {u8"http://download.ydstatic.cn/cidian/static/8.9/20201217/YoudaoDictSetup.exe",
      u8"~/TeemoTest/YoudaoDictSetup.exe", u8"e3323c940f0bc62fae3b0f32d7f2453b"},
 #endif

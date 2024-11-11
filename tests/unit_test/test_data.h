7e1c170839ce7d859031c60573e4f8f219811288
winsoft666 <mioktiar@gmail.com>
2024-11-11 12:19:57 +0800

    Using http test data.

--- tests/unit_test/test_data.h
+++ tests/unit_test/test_data.h
@@ -9,8 +9,8 @@ typedef struct _TestData {
 } TestData;
 
 static std::vector<TestData> http_test_datas = {
-    {u8"https://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_3.0.5.203184_64.exe", u8"./TeemoTest/NeteaseCloudMusic_Music_official_3.0.5.203184_64.exe", u8"2392d3bdc0726879da24b9b30d1b3faa"},
-    {u8"https://dldir1.qq.com/qqfile/qq/QQNT/Mac/QQ_6.9.59_241104_01.dmg", u8"./TeemoTest/QQ_6.9.59_241104_01.dmg", u8"bc809f476bf822dac35ea5009475b9eb"},
+    {u8"http://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_3.0.5.203184_64.exe", u8"./TeemoTest/NeteaseCloudMusic_Music_official_3.0.5.203184_64.exe", u8"2392d3bdc0726879da24b9b30d1b3faa"},
+    {u8"http://dldir1.qq.com/qqfile/qq/QQNT/Mac/QQ_6.9.59_241104_01.dmg", u8"./TeemoTest/QQ_6.9.59_241104_01.dmg", u8"bc809f476bf822dac35ea5009475b9eb"},
 };
 
 static std::vector<TestData> ftp_test_datas = {
